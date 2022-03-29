56d56da3b7bc979fa5e88b771d95cfdefc758aeb
jade_xiaohui <sjh86126@yeah.net>
2022-03-30 01:24:08 +0800

    first commit

---
+++ JHIMComponets/Classes/JHIMComponet.h
@@ -0,0 +1,18 @@
+//
+//  JHIMComponet.h
+//  JHIMComponets
+//
+//  Created by jinhui song on 2022/3/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JHIMComponet : NSObject
+
+- (void)printJHIMComponet;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JHIMComponets/Classes/JHIMComponet.m
@@ -0,0 +1,15 @@
+//
+//  JHIMComponet.m
+//  JHIMComponets
+//
+//  Created by jinhui song on 2022/3/30.
+//
+
+#import "JHIMComponet.h"
+
+@implementation JHIMComponet
+
+- (void)printJHIMComponet {
+    NSLog(@"printJHIMComponet*****");
+}
+@end
