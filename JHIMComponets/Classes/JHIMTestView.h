28aff1489e4a5589951c741b8bf8be8c01408770
jade_xiaohui <sjh86126@yeah.net>
2022-03-31 18:15:46 +0800

    test
    添加sdimage库

---
+++ JHIMComponets/Classes/JHIMTestView.h
@@ -0,0 +1,18 @@
+//
+//  JHIMTestView.h
+//  JHIMComponets
+//
+//  Created by jinhui song on 2022/3/31.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JHIMTestView : UIView
+
+- (void)showIMSDWebImageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JHIMComponets/Classes/JHIMTestView.m
@@ -0,0 +1,25 @@
+//
+//  JHIMTestView.m
+//  JHIMComponets
+//
+//  Created by jinhui song on 2022/3/31.
+//
+
+#import "JHIMTestView.h"
+#import <SDWebImage/SDWebImage.h>
+
+@implementation JHIMTestView
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+}
+
+- (void)showIMSDWebImageView {
+    UIImageView *imgView = [[UIImageView alloc] init];
+    [imgView setFrame:self.frame];
+    NSString *strUrl = @"https://github.com/JadeXH/luatest/blob/master/1111.jpg";
+    [imgView sd_setImageWithURL:[NSURL URLWithString:strUrl ? strUrl:@""] placeholderImage:nil];
+    [self addSubview:imgView];
+}
+
+@end
